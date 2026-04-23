62b76615df87fd126d762dcd8a6a1683acd5a89f
massiki <fikri.amrulloh15@gmail.com>
2026-04-23 21:32:53 +0700

    configuration ui layout

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
