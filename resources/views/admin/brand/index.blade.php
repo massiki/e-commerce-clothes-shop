b1561efabfa48844f6321506bec4d40383b19fa2
massiki <fikri.amrulloh15@gmail.com>
2026-04-25 17:41:39 +0700

    fix position code content layout admin

--- resources/views/admin/brand/index.blade.php
+++ resources/views/admin/brand/index.blade.php
@@ -1,114 +1,107 @@
 @extends('layouts.admin')
 
 @section('content')
-  <div class="main-content">
-
-    <div class="main-content-inner">
-      <div class="main-content-wrap">
-        <div class="flex items-center flex-wrap justify-between gap20 mb-27">
-          <h3>Brands</h3>
-          <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
-            <li>
-              <a href="index.html">
-                <div class="text-tiny">Dashboard</div>
-              </a>
-            </li>
-            <li>
-              <i class="icon-chevron-right"></i>
-            </li>
-            <li>
-              <div class="text-tiny">Brands</div>
-            </li>
-          </ul>
-        </div>
-
-        <div class="wg-box">
-          <div class="flex items-center justify-between gap10 flex-wrap">
-            <div class="wg-filter flex-grow">
-              <form class="form-search">
-                <fieldset class="name">
-                  <input type="text" placeholder="Search here..." class="" name="name" tabindex="2"
-                    value="" aria-required="true" required="">
-                </fieldset>
-                <div class="button-submit">
-                  <button class="" type="submit"><i class="icon-search"></i></button>
-                </div>
-              </form>
-            </div>
-            <a class="tf-button style-1 w208" href="{{ route('admin.brands.create') }}"><i class="icon-plus"></i>Add
-              new</a>
-          </div>
-
-          <x-alert-success status="success" />
-
-          <div class="wg-table table-all-user">
-            <div class="table-responsive">
-              <table class="table table-striped table-bordered">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Slug</th>
-                    <th>Images</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  @forelse ($brands as $brand)
-                    <tr>
-                      <td>{{ $loop->iteration }}</td>
-                      <td class="pname">
-                        <div class="image">
-                          <img src="{{ asset('storage/' . $brand->image) }}" alt="{{ $brand->name }}" class="image">
-                        </div>
-                        <div class="name">
-                          <a href="#" class="body-title-2">{{ $brand->name }}</a>
-                        </div>
-                      </td>
-                      <td>{{ $brand->slug }}</td>
-                      <td><a href="#" target="_blank">1</a></td>
-                      <td>
-                        <div class="list-icon-function">
-                          <a href="{{ route('admin.brands.edit', $brand->id) }}">
-                            <div class="item edit">
-                              <i class="icon-edit-3"></i>
-                            </div>
-                          </a>
-                          <form action="{{ route('admin.brands.destroy', $brand->id) }}" method="POST"
-                            onsubmit="return confirm('Are you sure you want to delete this brand?');">
-                            @method('delete')
-                            @csrf
-                            <button type="submit" class="item text-danger delete"
-                              style="border: none; background: transparent; padding: 0; cursor: pointer;">
-                              <i class="icon-trash-2"></i>
-                            </button>
-                          </form>
-
-                        </div>
-                      </td>
-                    </tr>
-                  @empty
-                    <tr>
-                      <td colspan="5" class="text-center">Belum ada brands</td>
-                    </tr>
-                  @endforelse
-
-                </tbody>
-              </table>
-            </div>
-            <div class="divider"></div>
-            <div class="flex items-center justify-between flex-wrap gap10 wgp-pagination">
-            </div>
-          </div>
-        </div>
+  <div class="main-content-inner">
+    <div class="main-content-wrap">
+      <div class="flex items-center flex-wrap justify-between gap20 mb-27">
+        <h3>Brands</h3>
+        <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
+          <li>
+            <a href="index.html">
+              <div class="text-tiny">Dashboard</div>
+            </a>
+          </li>
+          <li>
+            <i class="icon-chevron-right"></i>
+          </li>
+          <li>
+            <div class="text-tiny">Brands</div>
+          </li>
+        </ul>
       </div>
-      <div class="mt-5">
-        {{ $brands->links('pagination::bootstrap-5') }}
+
+      <div class="wg-box">
+        <div class="flex items-center justify-between gap10 flex-wrap">
+          <div class="wg-filter flex-grow">
+            <form class="form-search">
+              <fieldset class="name">
+                <input type="text" placeholder="Search here..." class="" name="name" tabindex="2"
+                  value="" aria-required="true" required="">
+              </fieldset>
+              <div class="button-submit">
+                <button class="" type="submit"><i class="icon-search"></i></button>
+              </div>
+            </form>
+          </div>
+          <a class="tf-button style-1 w208" href="{{ route('admin.brands.create') }}"><i class="icon-plus"></i>Add
+            new</a>
+        </div>
+
+        <x-alert-success status="success" />
+
+        <div class="wg-table table-all-user">
+          <div class="table-responsive">
+            <table class="table table-striped table-bordered">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Slug</th>
+                  <th>Images</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                @forelse ($brands as $brand)
+                  <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td class="pname">
+                      <div class="image">
+                        <img src="{{ asset('storage/' . $brand->image) }}" alt="{{ $brand->name }}" class="image">
+                      </div>
+                      <div class="name">
+                        <a href="#" class="body-title-2">{{ $brand->name }}</a>
+                      </div>
+                    </td>
+                    <td>{{ $brand->slug }}</td>
+                    <td><a href="#" target="_blank">1</a></td>
+                    <td>
+                      <div class="list-icon-function">
+                        <a href="{{ route('admin.brands.edit', $brand->id) }}">
+                          <div class="item edit">
+                            <i class="icon-edit-3"></i>
+                          </div>
+                        </a>
+                        <form action="{{ route('admin.brands.destroy', $brand->id) }}" method="POST"
+                          onsubmit="return confirm('Are you sure you want to delete this brand?');">
+                          @method('delete')
+                          @csrf
+                          <button type="submit" class="item text-danger delete"
+                            style="border: none; background: transparent; padding: 0; cursor: pointer;">
+                            <i class="icon-trash-2"></i>
+                          </button>
+                        </form>
+
+                      </div>
+                    </td>
+                  </tr>
+                @empty
+                  <tr>
+                    <td colspan="5" class="text-center">Belum ada brands</td>
+                  </tr>
+                @endforelse
+
+              </tbody>
+            </table>
+          </div>
+          <div class="divider"></div>
+          <div class="flex items-center justify-between flex-wrap gap10 wgp-pagination">
+          </div>
+        </div>
       </div>
     </div>
-
-    <div class="bottom-page">
-      <div class="body-text">Copyright © 2024 SurfsideMedia</div>
+    <div class="mt-5">
+      {{ $brands->links('pagination::bootstrap-5') }}
     </div>
   </div>
 @endsection
--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -1,86 +1,81 @@
 @extends('layouts.admin')
 
 @section('content')
-  <div class="main-content">
-    <div class="main-content-inner">
-      <div class="main-content-wrap">
-        <div class="flex items-center flex-wrap justify-between gap20 mb-27">
-          <h3>Edit Category</h3>
-          <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
-            <li>
-              <a href="#">
-                <div class="text-tiny">Dashboard</div>
-              </a>
-            </li>
-            <li>
-              <i class="icon-chevron-right"></i>
-            </li>
-            <li>
-              <a href="#">
-                <div class="text-tiny">Categories</div>
-              </a>
-            </li>
-            <li>
-              <i class="icon-chevron-right"></i>
-            </li>
-            <li>
-              <div class="text-tiny">Update Categories</div>
-            </li>
-          </ul>
-        </div>
-
-        <div class="wg-box">
-          <form class="form-new-product form-style-1" action="{{ route('admin.categories.update', $category->id) }}"
-            method="POST" enctype="multipart/form-data">
-            @method('patch')
-            @csrf
-            <fieldset class="name">
-              <div class="body-title">Category Name <span class="tf-color-1">*</span></div>
-              <input class="flex-grow" type="text" placeholder="Category name" name="name" tabindex="0"
-                value="{{ old('name', $category->name) }}" aria-required="true" required="">
-            </fieldset>
-            @error('name')
-              <div class="text-danger text-xs mt-1">{{ $message }}</div>
-            @enderror
-            <fieldset>
-              <div class="body-title">Upload images <span class="tf-color-1">*</span>
-              </div>
-              <div class="upload-image flex-grow">
-                <div class="item" id="imgpreview"
-                  style="display:{{ old('image') || $category->image || $errors->has('image') ? 'block' : 'none' }}">
-                  <img id="preview-image"
-                    src="{{ old('image') ? '' : ($category->image ? asset('storage/' . $category->image) : '') }}"
-                    class="effect8" alt="">
-                </div>
-
-                <div id="upload-file" class="item up-load">
-                  <label class="uploadfile" for="myFile">
-                    <span class="icon">
-                      <i class="icon-upload-cloud"></i>
-                    </span>
-                    <span class="body-text">Drop your images here or select <span class="tf-color">click to
-                        browse</span></span>
-                    <input type="file" id="myFile" name="image" accept="image/*">
-                  </label>
-                </div>
-              </div>
-            </fieldset>
-            @error('image')
-              <div class="text-danger text-xs mt-1">{{ $message }}</div>
-            @enderror
-
-            <div class="bot">
-              <div></div>
-              <button class="tf-button w208" type="submit">Save</button>
-            </div>
-          </form>
-
-
-        </div>
+  <div class="main-content-inner">
+    <div class="main-content-wrap">
+      <div class="flex items-center flex-wrap justify-between gap20 mb-27">
+        <h3>Edit Category</h3>
+        <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
+          <li>
+            <a href="#">
+              <div class="text-tiny">Dashboard</div>
+            </a>
+          </li>
+          <li>
+            <i class="icon-chevron-right"></i>
+          </li>
+          <li>
+            <a href="#">
+              <div class="text-tiny">Categories</div>
+            </a>
+          </li>
+          <li>
+            <i class="icon-chevron-right"></i>
+          </li>
+          <li>
+            <div class="text-tiny">Update Categories</div>
+          </li>
+        </ul>
       </div>
-    </div>
-    <div class="bottom-page">
-      <div class="body-text">Copyright © 2024 SurfsideMedia</div>
+
+      <div class="wg-box">
+        <form class="form-new-product form-style-1" action="{{ route('admin.categories.update', $category->id) }}"
+          method="POST" enctype="multipart/form-data">
+          @method('patch')
+          @csrf
+          <fieldset class="name">
+            <div class="body-title">Category Name <span class="tf-color-1">*</span></div>
+            <input class="flex-grow" type="text" placeholder="Category name" name="name" tabindex="0"
+              value="{{ old('name', $category->name) }}" aria-required="true" required="">
+          </fieldset>
+          @error('name')
+            <div class="text-danger text-xs mt-1">{{ $message }}</div>
+          @enderror
+          <fieldset>
+            <div class="body-title">Upload images <span class="tf-color-1">*</span>
+            </div>
+            <div class="upload-image flex-grow">
+              <div class="item" id="imgpreview"
+                style="display:{{ old('image') || $category->image || $errors->has('image') ? 'block' : 'none' }}">
+                <img id="preview-image"
+                  src="{{ old('image') ? '' : ($category->image ? asset('storage/' . $category->image) : '') }}"
+                  class="effect8" alt="">
+              </div>
+
+              <div id="upload-file" class="item up-load">
+                <label class="uploadfile" for="myFile">
+                  <span class="icon">
+                    <i class="icon-upload-cloud"></i>
+                  </span>
+                  <span class="body-text">Drop your images here or select <span class="tf-color">click to
+                      browse</span></span>
+                  <input type="file" id="myFile" name="image" accept="image/*">
+                </label>
+              </div>
+            </div>
+          </fieldset>
+          @error('image')
+            <div class="text-danger text-xs mt-1">{{ $message }}</div>
+          @enderror
+
+          <div class="bot">
+            <div></div>
+            <button class="tf-button w208" type="submit">Save</button>
+          </div>
+        </form>
+
+
+      </div>
     </div>
   </div>
   @push('scripts')
