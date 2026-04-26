8452b4dbf192193e9006f8e270997257f127c78c
massiki <fikri.amrulloh15@gmail.com>
2026-04-26 14:50:05 +0700

    add fitur edit product in admin

--- database/migrations/2026_04_25_135942_create_products_table.php
+++ database/migrations/2026_04_25_135942_create_products_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->string('slug')->unique();
             $table->string('short_description')->nullable();
             $table->text('description');
-            $table->decimal('regular_price');
-            $table->decimal('sale_price')->nullable();
+            $table->integer('regular_price');
+            $table->integer('sale_price')->nullable();
             $table->string('SKU');
             $table->enum('stock_status', ['instock', 'outofstock']);
             $table->boolean('featured')->default(false);
