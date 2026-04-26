eba40f31f7a32529aaeec3483ec73f7c05e3de93
Fikri Amrullah <152844321+massiki@users.noreply.github.com>
2026-04-26 19:35:24 +0700

    add create shop page (#15)

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ShopController extends Controller
+{
+    public function index()
+    {
+        $products = Product::latest('id')->paginate(9);
+        return view('shop', compact('products'));
+    }
+}
