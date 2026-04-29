489a75af9d0bed3c988bc937d47e3240f3f3237a
massiki <fikri.amrulloh15@gmail.com>
2026-04-29 19:57:36 +0700

    add show order in admin

---
+++ app/Http/Controllers/admin/OrderController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        $orders = Order::latest('id')->paginate(10);
+        return view('admin.order.index', compact('orders'));
+    }
+}
