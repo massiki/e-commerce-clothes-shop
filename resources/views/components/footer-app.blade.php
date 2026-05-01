fe8c61c1f40c40d65eee08bf69c26aac6f4544af
Fikri Amrullah <152844321+massiki@users.noreply.github.com>
2026-05-01 10:11:10 +0700

    replace link medsos (#50)

--- resources/views/components/footer-app.blade.php
+++ resources/views/components/footer-app.blade.php
@@ -7,50 +7,31 @@
             <img src="{{ asset('assets/images/logo.png') }}" alt="SurfsideMedia" class="logo__image d-block" />
           </a>
         </div>
-        <p class="footer-address">123 Beach Avenue, Surfside City, CA 00000</p>
-        <p class="m-0"><strong class="fw-medium">contact@surfsidemedia.in</strong></p>
-        <p><strong class="fw-medium">+1 000-000-0000</strong></p>
+        <p class="footer-address">Kp. Cigadog Rt. 01 Rw. 10, Desa Lingkungpasir, Kec. Cibiuk, Kab. Garut</p>
+        <p class="m-0"><strong class="fw-medium">fikri.amrulloh15@gmail.com</strong></p>
+        <p class="m-0"><strong class="fw-medium">224260064.mhs@stmikjabar.ac.id</strong></p>
+        <p><strong class="fw-medium">+62-852-9453-2451</strong></p>
 
         <ul class="social-links list-unstyled d-flex flex-wrap mb-0">
           <li>
-            <a href="#" class="footer__social-link d-block">
-              <svg class="svg-icon svg-icon_facebook" width="9" height="15" viewBox="0 0 9 15"
-                xmlns="http://www.w3.org/2000/svg">
-                <use href="#icon_facebook" />
-              </svg>
+            <a href="https://www.linkedin.com/in/fikri-amrullah-5583b52b6/" target="_blank"
+              class="footer__social-link d-block">
+              <i class="fa fa-linkedin"></i>
             </a>
           </li>
           <li>
-            <a href="#" class="footer__social-link d-block">
-              <svg class="svg-icon svg-icon_twitter" width="14" height="13" viewBox="0 0 14 13"
-                xmlns="http://www.w3.org/2000/svg">
-                <use href="#icon_twitter" />
-              </svg>
+            <a href="https://wa.me/6285294532451" target="_blank" class="footer__social-link d-block">
+              <i class="fa fa-whatsapp"></i>
             </a>
           </li>
           <li>
-            <a href="#" class="footer__social-link d-block">
-              <svg class="svg-icon svg-icon_instagram" width="14" height="13" viewBox="0 0 14 13"
-                xmlns="http://www.w3.org/2000/svg">
-                <use href="#icon_instagram" />
-              </svg>
+            <a href="https://www.instagram.com/fikri.amrulloh.15" target="_blank" class="footer__social-link d-block">
+              <i class="fa fa-github"></i>
             </a>
           </li>
           <li>
-            <a href="#" class="footer__social-link d-block">
-              <svg class="svg-icon svg-icon_youtube" width="16" height="11" viewBox="0 0 16 11"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M15.0117 1.8584C14.8477 1.20215 14.3281 0.682617 13.6992 0.518555C12.5234 0.19043 7.875 0.19043 7.875 0.19043C7.875 0.19043 3.19922 0.19043 2.02344 0.518555C1.39453 0.682617 0.875 1.20215 0.710938 1.8584C0.382812 3.00684 0.382812 5.46777 0.382812 5.46777C0.382812 5.46777 0.382812 7.90137 0.710938 9.07715C0.875 9.7334 1.39453 10.2256 2.02344 10.3896C3.19922 10.6904 7.875 10.6904 7.875 10.6904C7.875 10.6904 12.5234 10.6904 13.6992 10.3896C14.3281 10.2256 14.8477 9.7334 15.0117 9.07715C15.3398 7.90137 15.3398 5.46777 15.3398 5.46777C15.3398 5.46777 15.3398 3.00684 15.0117 1.8584ZM6.34375 7.68262V3.25293L10.2266 5.46777L6.34375 7.68262Z" />
-              </svg>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="footer__social-link d-block">
-              <svg class="svg-icon svg-icon_pinterest" width="14" height="15" viewBox="0 0 14 15"
-                xmlns="http://www.w3.org/2000/svg">
-                <use href="#icon_pinterest" />
-              </svg>
+            <a href="https://web.facebook.com/fikri.amrulloh.56" target="_blank" class="footer__social-link d-block">
+              <i class="fa fa-facebook"></i>
             </a>
           </li>
         </ul>
@@ -111,7 +92,7 @@
 
   <div class="footer-bottom">
     <div class="container d-md-flex align-items-center">
-      <span class="footer-copyright me-auto">©2024 Surfside Media</span>
+      <span class="footer-copyright me-auto">Copyright © {{ date('Y') }} Fikri Amrullah</span>
       <div class="footer-settings d-md-flex align-items-center">
         <a href="privacy-policy.html">Privacy Policy</a> &nbsp;|&nbsp; <a href="terms-conditions.html">Terms &amp;
           Conditions</a>
